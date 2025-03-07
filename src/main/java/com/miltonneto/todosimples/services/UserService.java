88a7a674175df02631e5b750100698a94f916c1b
Milton <miltonneto209@gmail.com>
2025-03-06 22:23:43 -0300

    Create userService

---
+++ src/main/java/com/miltonneto/todosimples/services/UserService.java
@@ -0,0 +1,53 @@
+package com.miltonneto.todosimples.services;
+
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.miltonneto.todosimples.models.User;
+import com.miltonneto.todosimples.repositories.TaskRepository;
+import com.miltonneto.todosimples.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TaskRepository TaskRepository;
+
+
+    public User findById(Long id){  //optional , caso o usuario esteja vazio.
+        Optional<User> user = this.userRepository.findById(id);
+        return user.orElseThrow(() -> new RuntimeException(  //exeption padrao interrompe a aplicacao , RuntimeExeption nao. Retorna o usuario se esle estiver preenchido se nao faz uma throwExeption
+            "Usuario nao encontrado! ID:" + id + "Tipo: " + User.class.getName()
+        ));
+    }
+
+    @Transactional
+    public User create (User obj ){ 
+        obj.setId(null);
+        obj = this.userRepository.save(obj);
+        this.TaskRepository.saveAll(obj.getTasks());
+        return obj;
+    }
+
+    @Transactional
+    public User update(User obj){
+        User newObj = findById(obj.getId());
+        newObj.setPassword(obj.getPassword());
+        return this.userRepository.save(newObj);
+    }
+
+    public void delete(Long id){
+        findById(id);
+        try {
+            this.userRepository.deleteById(id);
+        } catch (Exception e) {
+            throw new RuntimeException("Nao e possivel excluir pois ha entidades relacionadas !!! ");
+        }
+    }
+
+}
