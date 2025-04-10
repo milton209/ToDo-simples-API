f9059659848c676c250a81604f925d033340d29d
Milton <miltonneto209@gmail.com>
2025-04-10 16:25:58 -0300

    add lombok

--- src/main/java/com/miltonneto/todosimples/models/Task.java
+++ src/main/java/com/miltonneto/todosimples/models/Task.java
@@ -11,104 +11,37 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 
 @Entity
-@Table (name = Task.TABLE_NAME)
+@Table(name = Task.TABLE_NAME)
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
 
 public class Task {
     public static final String TABLE_NAME = "task";
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id" , unique = true)
+    @Column(name = "id", unique = true)
     private Long id;
-    
+
     @ManyToOne
     @JoinColumn(name = "user_id")
     private User user;
 
-    @Column(name = "description" , length = 255 , nullable = false)
+    @Column(name = "description", length = 255, nullable = false)
     @NotNull
     @NotEmpty
-    @Size(min = 1 , max = 255)
+    @Size(min = 1, max = 255)
     private String description;
-
-    public Task() {
-    }
-
-    public Task(long id, User user, String description) {
-        this.id = id;
-        this.user = user;
-        this.description = description;
-    }
-
-    public long getId() {
-        return this.id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public User getUser() {
-        return this.user;
-    }
-
-    public void setUser(User user) {
-        this.user = user;
-    }
-
-    public String getDescription() {
-        return this.description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Task id(long id) {
-        setId(id);
-        return this; 
-    }
-
-    public Task user(User user) {
-        setUser(user);
-        return this;
-    }
-
-    public Task description(String description) {
-        setDescription(description);
-        return this;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) 
-        return true;
     
-        if (obj == null) {
-        return false;
-        }
-    
-        if (!(obj instanceof Task)) 
-        return false;
-        Task other = (Task) obj;
-    
-            if (this.id != null)
-            return false;
-    
-            else if (! this.id.equals(other.id))
-            return false;
-        return Objects.equals(this.id , other.id) && Objects.equals(this.user , other.user) && Objects.equals (this.description, other.description);
-    }
-    
-        @Override 
-        public int hashCode() {
-            final int prime = 31;
-            int result = 1;
-            result = prime * result + ((this.id == null) ? 0 : this.id.hashCode());
-            return result; 
-        }
-
 }
